3ae9ec91b53d5bce30f411c949d1170f0cafb7c7
GLima727 <guilhermegutierrez727@gmail.com>
2022-04-27 19:21:37 +0100

    fixes to the looks

--- src/Todo.js
+++ src/Todo.js
@@ -9,7 +9,7 @@ export default function Todo({ todo, toggleTodo }) {
   return (
     <div className='tasks'>
       <label>
-        <input type="checkbox" class="checkbox-effect checkbox-effect-4" id="get-up-4" value="get-up-4" name="get-up-4" checked={todo.complete} onChange={handleTodoClick} />
+        <input type="checkbox" class="checkbox" checked={todo.complete} onChange={handleTodoClick} />
         {todo.name}
       </label>
     </div>
